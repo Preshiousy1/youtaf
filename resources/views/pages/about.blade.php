55f813d0b9319d8c1c6b2ef52aee6a22d97906ac
Precious Atam <precious4prestige@gmail.com>
2020-11-14 18:51:13 +0100

    fixed the bug on about page again

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,6 +5,18 @@
 @endsection
 
 @section('content')
+<?php
+    function phoneNum($num){
+        // $data = '+11234567890';
+        $data = $num;
+
+        if(  preg_match( '/^\+(\d{1})(\d{3})(\d{3})(\d{4})$/', $data,  $matches ) )
+        {
+            $result = $matches[1].' ('.$matches[2] . ') ' .$matches[3] . '-' . $matches[4];
+            return $result;
+        }
+    }
+?>
      @include('partials.about-page')
      @include('partials.donate')
      @include('partials.contact')
