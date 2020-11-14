ed0d25016be4fb9fddf27f7a7468d29a2a8b0754
Precious Atam <precious4prestige@gmail.com>
2020-11-14 18:29:53 +0100

    removed link on author name

--- resources/views/partials/blog-section.blade.php
+++ resources/views/partials/blog-section.blade.php
@@ -14,7 +14,7 @@
               <div>
                 <a href="single.html" class="mb-4 d-block"><img src="/storage/cover_images/{{$posts[0]->cover_image}}" style="height: 300px !important;" alt="Image" class="img-fluid rounded"></a>
                 <h2><a href="/single/{{$posts[0]->id}}">{{$posts[0]->title}}</a></h2>
-                <p class="text-muted mb-3 text-uppercase small"><span class="mr-2">{{$posts[0]->created_at}} </span> By <a href="single.html" class="by">{{$posts[0]->user->name}}</a></p>
+                <p class="text-muted mb-3 text-uppercase small"><span class="mr-2">{{$posts[0]->created_at}} </span> By <span  class="by">{{$posts[0]->user->name}}</span></p>
                 <p> {!! Str::words($posts[0]->body, 10,'...')  !!} </p>
                 <p><a href="/single/{{$posts[0]->id}}">Read More</a></p>
               </div>
@@ -25,7 +25,7 @@
                 <div>
                   <a href="single.html" class="mb-4 d-block"><img src="/storage/cover_images/{{$posts[0]->cover_image}}" style="height: 300px !important;" alt="Image" class="img-fluid rounded"></a>
                   <h2><a href="/single/{{$posts[0]->id}}">{{$posts[0]->title}}</a></h2>
-                  <p class="text-muted mb-3 text-uppercase small"><span class="mr-2">{{$posts[0]->created_at}} </span> By <a href="single.html" class="by">{{$posts[0]->user->name}}</a></p>
+                  <p class="text-muted mb-3 text-uppercase small"><span class="mr-2">{{$posts[0]->created_at}} </span> By <span class="by">{{$posts[0]->user->name}}</span></p>
                   <p> {!! Str::words($posts[0]->body, 10,'...')  !!} </p>
                   <p><a href="/single/{{$posts[0]->id}}">Read More</a></p>
                 </div>
@@ -34,7 +34,7 @@
                 <div>
                   <a href="single.html" class="mb-4 d-block"><img src="/storage/cover_images/{{$posts[1]->cover_image}}" alt="Image" style="height: 300px !important;" class="img-fluid rounded"></a>
                   <h2><a href="/single/{{$posts[1]->id}}">{{$posts[1]->title}}</a></h2>
-                  <p class="text-muted mb-3 text-uppercase small"><span class="mr-2">{{$posts[1]->created_at}} </span> By <a href="single.html" class="by">{{$posts[1]->user->name}}</a></p>
+                  <p class="text-muted mb-3 text-uppercase small"><span class="mr-2">{{$posts[1]->created_at}} </span> By <span class="by">{{$posts[1]->user->name}}</span></p>
                   <p> {!! Str::words($posts[1]->body, 10,'...')  !!}</p>
                   <p><a href="/single/{{$posts[1]->id}}">Read More</a></p>
                 </div>
